e909294f43660d386520064dc850f42bc55cfd52
scjackman <samjackman10@hotmail.co.uk>
2025-05-30 22:35:31 -0700

    Adding config logic to app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
 @app.route("/")
 def index():
-    return "This is V2.0 of my web app!"
-
-
-
+    secret_key = app.config.get("SECRET_KEY")
+    return f"The configured secret key is {secret_key}."
