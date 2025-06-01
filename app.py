882a1544320e3ffd264393f65f04d0fa58aa6fc5
scjackman <samjackman10@hotmail.co.uk>
2025-05-31 17:36:45 -0700

    Connected frontend and backend to create working webapp

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+import yfinance as yf
 
 app = Flask(__name__)
 env_config = os.getenv('APP_SETTINGS', 'config.DevelopmentConfig')
 app.config.from_object(env_config)
 
 def retrieve_ticker_bid(ticker: str) -> float:
-    
     # Retrieve live bid info from the Yahoo Finance ticker passed to the function
-    # Required package
-    import yfinance as yf
-    
-    # Retrieve ticker data using yfinance
     try:
         ticker_response = yf.Ticker(ticker)
 
@@ -19,19 +15,22 @@ def retrieve_ticker_bid(ticker: str) -> float:
         print('retrieve_ticker_bid: The function could not retrieve the ticker info from yfinance.')
 
     # Obtain ticker 'bid' info from data, and return
+    return ticker_response.info['bid']
 
-    ticker_bid = ticker_response.info['bid']
-
-    return {
-        'ticker_bid': ticker_bid
-    }
 
 @app.route('/')
 def index():
     secret_key = app.config.get('SECRET_KEY')
-    return f'''
+    return render_template('index.html')
 
-        This webapp confirms that Olivia is a {secret_key} gal!
-        Oh and btw, the current GBP -> CAD exchange rate is {retrieve_ticker_bid('GBPCAD=X')['ticker_bid']}.
-    
-    '''
+@app.route('/convert',methods=['POST'],endpoint='convert_value')
+def convert_value():
+    data = request.get_json()
+    amount = float(data['amount'])
+    from_currency = data['from']
+    to_currency = data['to']
+
+    ticker = from_currency + to_currency + '=X'
+    converted_amount  = amount * retrieve_ticker_bid(ticker)
+
+    return jsonify({'converted_amount': converted_amount})
