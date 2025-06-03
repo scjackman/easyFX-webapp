4a8c0c21f5a8ff988fc50ef31227e25fcbd2ceaf
scjackman <samjackman10@hotmail.co.uk>
2025-06-02 22:39:22 -0700

    Fixed config error

--- config.py
+++ config.py
@@ -1,24 +1,12 @@
 import os
-import secrets
 
 class Config:
     DEBUG = False
     DEVELOPMENT = False
-    
-    @staticmethod
-    def generate_secret_key():
-        """Generate a secure random secret key."""
-        return secrets.token_hex(32)
-    
-    # Use environment variable if set, otherwise generate a new key
-    # In production, ALWAYS set FLASK_SECRET_KEY environment variable
-    SECRET_KEY = os.getenv('FLASK_SECRET_KEY', generate_secret_key.__func__())
+    SECRET_KEY = os.getenv('SECRET_KEY', 'this-is-the-default-secret-key')
 
 class ProductionConfig(Config):
-    # Production should always use environment variable
-    SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
-    if not SECRET_KEY:
-        raise ValueError("FLASK_SECRET_KEY environment variable must be set in production!")
+    pass
 
 class DevelopmentConfig(Config):
     DEBUG = True
